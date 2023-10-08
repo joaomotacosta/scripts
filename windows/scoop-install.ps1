fe4ed850b2834f823084f39090cd93cd183a3e64
João da Costa <133066251+joaomotacosta@users.noreply.github.com>
2023-10-08 17:41:37 +0100

    EAPO + sudo

--- windows/scoop-install.ps1
+++ windows/scoop-install.ps1
@@ -1,8 +1,11 @@
 irm get.scoop.sh -outfile 'install.ps1'
 .\install.ps1 -ScoopDir 'D:\Applications\Scoop' -ScoopCacheDir 'D:\Applications\ScoopCache'
 scoop install aria2 git lessmsi
+scoop config aria2-warning-enabled false
 scoop config use_lessmsi $true
 scoop bucket add extras
 scoop bucket add nonportable
-scoop install 7zip adb equalizer-apo-np ffmpeg github libreoffice mpv.net msiafterburner nextcloud obs-studio picotorrent rustdesk scrcpy spotify vscode
+scoop bucket add games
+scoop install 7zip adb ffmpeg github libreoffice mpv.net msiafterburner obs-studio picotorrent rustdesk scrcpy spotify sudo vscode stremio signal rtss
+sudo scoop install equalizer-apo-np
 pause
