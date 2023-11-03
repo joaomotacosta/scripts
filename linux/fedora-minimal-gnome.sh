d5eaf055f324f8b11c209e405efe7049264b50d9
João da Costa <133066251+joaomotacosta@users.noreply.github.com>
2023-11-03 23:21:45 +0000

    - Comments - Added TLP and Powertop

--- linux/fedora-minimal-gnome.sh
+++ linux/fedora-minimal-gnome.sh
@@ -3,51 +3,61 @@ echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
 echo 'max_parallel_downloads=10'| sudo tee -a /etc/dnf/dnf.conf
 echo 'defaultyes=1' | sudo tee -a /etc/dnf/dnf.conf
 
-# Install GNOME base
+# GNOME - Base
 sudo dnf upgrade -y
 sudo dnf install @base-x gnome-shell gnome-terminal gnome-software gnome-tweaks nautilus neofetch -y
 
-# Extras - 'Open in Terminal', user-directories and GNOME extensions connector
+# GNOME - Extras
 sudo dnf install gnome-terminal-nautilus xdg-user-dirs xdg-user-dirs-gtk gnome-browser-connector unzip -y
 
-# Firmware - Intel Wifi (Make sure you have an internet connection)
+# GNOME - Apps
+sudo dnf install gnome-extensions-app gnome-calculator gnome-disk-utility gedit -y
+
+# Hardware - Intel Wifi (Make sure you have an internet connection)
 sudo dnf install iwl7260-firmware -y
 
-# intel-undervolt
+# Hardware - intel-undervolt
 sudo dnf install intel-undervolt -y
 
-# auto-cpufreq
+# Battery - TLP
+sudo dnf install tlp tlp-rdw -y
+sudo dnf remove power-profiles-daemon -y
+sudo systemctl enable tlp.service
+sudo systemctl mask system-rdfkill.service systemd-rfkill.socket
+
+# Battery - auto-cpufreq
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq && sudo ./auto-cpufreq-installer
 
-# Virtualization
+# Battery - powertop
+sudo dnf install powertop -y
+sudo powertop --auto-tune
+
+# Apps - Virtualization
 sudo dnf install @virtualization -y
 
-# GNOME
-sudo dnf install gnome-extensions-app gnome-calculator gnome-disk-utility gedit -y
-
-# Brave
+# Apps - Brave
 sudo dnf install dnf-plugins-core
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser -y
 
-# LibreOffice
+# Apps - LibreOffice
 sudo dnf install libreoffice-calc libreoffice-impress libreoffice-writer -y
 
-# scrcpy
+# Apps - scrcpy
 sudo dnf install scrcpy -y
 
-# VScode
+# Apps - VScode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf check-update
 sudo dnf install code -y
 
-# Remove GNOME Tour
+# Apps - Remove GNOME Tour
 sudo dnf remove gnome-tour -y
 
-# RPMfusion
+# Apps - RPMfusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 # Flatpak
@@ -55,22 +65,18 @@ sudo dnf install flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub -y
 
-# Orchis theme
+# Theming - Orchis
 sudo dnf install gnome-themes-extra gtk-murrine-engine sassc -y
-# git clone https://github.com/vinceliuice/Orchis-theme
-# cd Orchis-theme
-# ./install.sh -t all -c dark -s compact -l --tweaks solid compact black primary macos submenu --shell 44
-# cd ~
+git clone https://github.com/vinceliuice/Orchis-theme
+cd Orchis-theme
+./install.sh -t all -c dark -s compact -l --tweaks solid compact black primary macos submenu
+cd ~
 
-# Tela icons
-# git clone https://github.com/vinceliuice/Tela-icon-theme
-# cd Tela-icon-theme
-# ./install.sh -a
-# cd ~
-
-# Theming
-git clone https://github.com/NeuronSooup/gnome-topbar-le-dots
-git clone https://github.com/rose-pine/gtk
+# Theming - Tela
+git clone https://github.com/vinceliuice/Tela-icon-theme
+cd Tela-icon-theme
+./install.sh -a
+cd ~
 
 # Set graphical target, autologin and reboot
 sudo systemctl set-default graphical.target
