9e36c0b2c4a3e03e9ae7479aae27bb37d9fb7371
João da Costa <133066251+joaomotacosta@users.noreply.github.com>
2023-12-28 01:59:20 +0000

    Waydroid installation for Debian-based X11 systems.

---
+++ linux/waydroid-install-debian-x11.sh
@@ -0,0 +1,8 @@
+# Dependencies
+sudo apt install curl ca-certificates weston -y
+
+# Adding Waydroid repo
+curl https://repo.waydro.id | sudo bash
+
+# Installing Waydroid
+sudo apt install waydroid -y
---
+++ linux/waydroid-run-debian-x11.sh
@@ -0,0 +1,10 @@
+# Stop Waydroid
+waydroid session stop
+sudo waydroid container stop
+
+# Weston
+waydroid session start & weston
+
+# A blank window will pop up
+# Press top left button to open terminal
+waydroid show-full-ui
